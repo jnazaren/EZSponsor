9e03e049d99b951635804861be9f6e88063f323a
Jacob Nazarenko <jnazaren@gmail.com>
2019-09-21 14:21:19 -0400

    UI update

--- app/src/main/java/com/test/ezsponsor/MainActivity.java
+++ app/src/main/java/com/test/ezsponsor/MainActivity.java
@@ -4,8 +4,15 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.TextView;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.auth.SignInResult;
+import com.microsoft.appcenter.crashes.Crashes;
+import com.microsoft.appcenter.auth.Auth;
+import com.microsoft.appcenter.utils.async.AppCenterConsumer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,9 +24,6 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
                     return true;
@@ -36,9 +40,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        AppCenter.start(getApplication(), "a24aa30c-220e-4534-a84e-b931c6cba18d",
+            Analytics.class, Crashes.class); // microsoft app center integration
+
+
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        // Attaching the layout to the toolbar object
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Setting toolbar as the ActionBar with setSupportActionBar() call
+        setSupportActionBar(toolbar);
     }
 
 }
