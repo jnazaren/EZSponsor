5e45e4f3092ef4f415fdf4ab2cf4ce70b08510ce
Devon Fazekas <thomas-d@hotmail.ca>
2019-09-22 04:34:28 -0400

    Fix home page

--- app/src/main/java/com/test/ezsponsor/HomeFragment.java
+++ app/src/main/java/com/test/ezsponsor/HomeFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,38 +13,44 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collection;
 
 public class HomeFragment extends Fragment {
 
     ListView listView;
     ArrayList<String> al;
     ArrayAdapter<String> aa;
+    ArrayList<CardView> cardViewList = null;
 
+    private View.OnClickListener openUserDetails = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(getActivity(), DonationInfo.class);
+            startActivity(intent);
+        }
+    };
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        listView = (ListView) view.findViewById(R.id.home_listView);
-        al = new ArrayList<String>();
-        aa = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_activated_1, al);
-        listView.setAdapter(aa);
-        al.add("Test 1");
-        al.add("Test 2");
-        al.add("Test 3");
 
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                // TODO: Add activity reference.
-//                Intent intent = new Intent(getActivity(), <activityName>.class);
-//                startActivity(intent);
-                Toast.makeText(getActivity().getApplicationContext(), "Test", Toast.LENGTH_SHORT).show();
-            }
-        });
+        CardView card1 = view.findViewById(R.id.card_1);
+        card1.setOnClickListener(openUserDetails);
+
+        CardView card2 = view.findViewById(R.id.card_2);
+        card2.setOnClickListener(openUserDetails);
+
+        CardView card3 = view.findViewById(R.id.card_3);
+        card3.setOnClickListener(openUserDetails);
+
+        CardView card4 = view.findViewById(R.id.card_4);
+        card4.setOnClickListener(openUserDetails);
+
+        CardView card5 = view.findViewById(R.id.card_5);
+        card5.setOnClickListener(openUserDetails);
+
 
         return view;
     }
